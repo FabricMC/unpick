c9348d25afc2338f52390199cb258646e3fb9085
joe <burtonjae@hotmail.co.uk>
2025-08-15 16:04:06 +0100

    Add ForwardingUnpickV3Visitor and improve docs of UnpickV3Remapper

---
+++ unpick-format-utils/src/main/java/daomephsta/unpick/constantmappers/datadriven/tree/ForwardingUnpickV3Visitor.java
@@ -0,0 +1,33 @@
+package daomephsta.unpick.constantmappers.datadriven.tree;
+
+import org.jetbrains.annotations.Nullable;
+
+public abstract class ForwardingUnpickV3Visitor extends UnpickV3Visitor {
+	@Nullable
+	protected final UnpickV3Visitor downstream;
+
+	public ForwardingUnpickV3Visitor(@Nullable UnpickV3Visitor downstream) {
+		this.downstream = downstream;
+	}
+
+	@Override
+	public void visitGroupDefinition(GroupDefinition groupDefinition) {
+		if (downstream != null) {
+			downstream.visitGroupDefinition(groupDefinition);
+		}
+	}
+
+	@Override
+	public void visitTargetField(TargetField targetField) {
+		if (downstream != null) {
+			downstream.visitTargetField(targetField);
+		}
+	}
+
+	@Override
+	public void visitTargetMethod(TargetMethod targetMethod) {
+		if (downstream != null) {
+			downstream.visitTargetMethod(targetMethod);
+		}
+	}
+}
